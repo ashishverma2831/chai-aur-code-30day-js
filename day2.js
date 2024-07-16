aa7bf312929900efae98fc4d2acc91b14de9e3b6
ashishverma2831 <ashishverma510207@gmail.com>
2024-07-16 13:21:30 +0530

    feat: Add code for Day 2 tasks on operators

---
+++ day2.js
@@ -0,0 +1,52 @@
+// Day 2 : Operators 
+
+let a = 10;
+let b = 20;
+
+// Task 1
+console.log(a+b);
+// Task 2
+console.log(a-b);
+// Task 3
+console.log(a*b);
+// Task 4
+console.log(a/b);
+
+// Task 5
+let c = 10;
+let d = 3;
+console.log(c%d);
+
+// Task 6
+let e = 10;
+let f = 3;
+
+console.log(e+=f);
+// Task 7
+console.log(e-=f);
+
+let g = 10;
+let h = 4;
+
+// Task 8
+console.log(g>h);
+console.log(g<h);
+
+// Task 9
+console.log(g>=h);
+console.log(g<=h);
+
+// Task 10
+console.log(g==h);
+console.log(g===h);
+
+// Task 11
+console.log(3>4 && 4>3);
+// Task 12
+console.log(3>4 || 4>3);
+// Task 13
+console.log(!(3>4) && 4>3);
+
+// Task 14
+let i = 10;
+console.log(i>=0? 'Positive':'Negative');
