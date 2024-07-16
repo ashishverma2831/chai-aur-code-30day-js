4a9c46aa7733d2c4ed2ffcde609d79413b642381
ashishverma2831 <ashishverma510207@gmail.com>
2024-07-16 12:08:30 +0530

    chore: Add code for Day 1 tasks on variables and data types

---
+++ day1.js
@@ -0,0 +1,38 @@
+// Day 1 : Variables and Data Types
+
+
+// Task 1
+var num = 10;
+console.log(num);
+
+// Task 2
+let str = 'Hello to this course on JavaScript';
+console.log(str);
+
+// Task 3
+const bool = true;
+console.log(bool);
+
+// Task 4
+a = 20;
+b = 'Hello';
+c = true;
+d = {
+    name:'Ashish Verma',
+    age: 23
+}
+e = [1,2,3,4,5];
+console.log(a,b,c,d,e);
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e);
+
+// Task 5
+let x = 10;
+console.log(x);
+x = 20;
+console.log(x);
+
+// Task 6
+const y = 10;
+console.log(y);
+// y = 20;
+// console.log(y);
