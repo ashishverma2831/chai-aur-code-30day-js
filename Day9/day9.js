d3ebf2375b0580a3f94f5abe28bb2865ee1ea2f2
ashishverma2831 <ashishverma510207@gmail.com>
2024-07-24 13:09:18 +0530

    feat: Update Day 9 HTML and JavaScript code

--- Day9/day9.js
+++ Day9/day9.js
@@ -1,23 +1,32 @@
 // Day 9 : DOM Manipulation
 
-// Task 1
+// const { JSDOM } = require('jsdom');   
+// const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
+// global.document = dom.window.document;
+
 // document.getElementById('a').textContent = "Hello World";
-// document.querySelectorAll('#a').textContent = "Hello World";
+
+// Task 1
+const text1 = document.getElementById('a').textContent = "Hello World 1";
 
 // Task 2
-// const text2 = document.getElementsByClassName('b');
-// text2.textContent = "Hello World 2";
+const text2 = document.getElementsByClassName('b');
+text2[0].textContent = "Hello World 2";
 
+// Task 3
+const text3 = document.createElement('div');
+text3.textContent = "Hello World 3";
+document.body.appendChild(text3);
 
-// if(typeof document !== undefined){
-//     console.log("Hello World");
-// }
-// else{
-//     console.log("DOM is not loaded yet");
-// }
+// Task 4
+const text4 = document.createElement('li');
+text4.textContent = "Hello World 4";
+document.getElementById('list').appendChild(text4);
 
-const { JSDOM } = require('jsdom');   
-const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
-global.document = dom.window.document;
+// Task 5
+const text5 = document.getElementById('c');
+document.body.removeChild(text5);
 
-document.getElementById('a').textContent = "Hello World";
+// Task 6
+const text6 = document.getElementById('d');
+document.body.lastChild.remove(text6);
